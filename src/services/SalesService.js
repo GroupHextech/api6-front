5b83a4c62c561bdcaa57a6ad1a6fb9fa721d25b3
Michel Rubens <michelrube@gmail.com>
2024-05-01 19:54:29 -0300

    API6-93 :sparkles: feat: Add state param

--- src/services/SalesService.js
+++ src/services/SalesService.js
@@ -1,4 +1,5 @@
 const API_URL = "http://3.140.192.18:8000"
+// const API_URL = "http://127.0.0.1:5000/"
 
 export async function getGender() {
     try {
@@ -26,17 +27,28 @@ export async function getSales() {
       }
 }
 
-export async function getCategories() {
-    try {
-        const response = await fetch(`${API_URL}/css/categories`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch categories');
-        }
-        const data = await response.json();
-        return data.list;
-      } catch (error) {
-        throw new Error(error.message);
+export async function getCategories(states) {
+  try {
+      let apiUrl = `${API_URL}/css/categories`;
+
+      // Adicionar parâmetros de consulta se forem fornecidos
+      if (states) {
+          apiUrl += "?";
+
+          if (states && states.length > 0) {
+              apiUrl += `&state=${states.join('&state=')}`;
+          }
       }
+
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+          throw new Error('Failed to fetch categories');
+      }
+      const data = await response.json();
+      return data.list;
+  } catch (error) {
+      throw new Error(error.message);
+  }
 }
 
 export async function getStates() {
