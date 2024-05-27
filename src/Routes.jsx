5c633b0f021958cf1c47fdee26cd19a5a9624e86
Michel Rubens <61568495+michelrubens@users.noreply.github.com>
2024-05-26 23:16:06 -0300

    Develop (#53)
    * API6-99 ✨ feat: Adicao da blacklist (#52)
    
    * Revert ":ok_hand: style: Resolvendo conflito de merge"
    
    This reverts commit fbb025f57efee4bd3e818b0df98c91501c193f79, reversing
    changes made to 6d242d14cc05004a2ee59371475859faed98ba0d.
    
    ---------
    
    Co-authored-by: HelenAlevato <61571753+HelenAlevato@users.noreply.github.com>
    Co-authored-by: Patrick Souza <patrickgabriel103f@gmail.com>

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -10,44 +10,43 @@ import Map from "../src/pages/Map";
 import Register from "../src/pages/auth/Register";
 import Login from "./pages/auth/Login";
 import UserData from "./pages/UserData";
-import { AuthContext } from "./services/authContext";
+import Manegement from "./pages/Manegement";
+import { AuthProvider } from "./services/authContext";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function AppRoutes() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(false);
+
   const location = useLocation();
   const isLoginPage = location.pathname === "/login";
   const isRegisterPage = location.pathname === "/register";
 
-  // Verificar se o usuário está autenticado 
-  const { isAuthenticated } = useContext(AuthContext);
-
-  // Função para renderizar as rotas privadas apenas se o usuário estiver autenticado
-  const renderPrivateRoute = (element) => {
-    return isAuthenticated ? element : <Navigate to="/login" />;
-  };
-
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="app">
-          {!isLoginPage && !isRegisterPage && <Sidebar isSidebar={isSidebar} />}
-          <main className="content" style={{overflowY: 'scroll'}}>
-            {!isLoginPage && !isRegisterPage && <Topbar setIsSidebar={setIsSidebar} />}
-
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/" element={renderPrivateRoute(<Home />)} />
-              <Route path="/dashboard" element={renderPrivateRoute(<Dashboard />)} />
-              <Route path="/map" element={renderPrivateRoute(<Map />)} />
-              <Route path="/userData" element={renderPrivateRoute(<UserData />)} />
-              {/* Adicione outras rotas aqui */}
-            </Routes>
-          </main>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <AuthProvider>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            {!isLoginPage && !isRegisterPage && <Sidebar isSidebar={isSidebar} />}
+            <main className="content" style={{ overflowY: 'scroll' }}>
+              {!isLoginPage && !isRegisterPage && <Topbar setIsSidebar={setIsSidebar} />}
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route element={<PrivateRoute />}>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/map" element={<Map />} />
+                  <Route path="/userData" element={<UserData />} />
+                  <Route path="/manege" element={<Manegement />} />
+                </Route>
+                {/* Adicione outras rotas aqui */}
+              </Routes>
+            </main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </AuthProvider>
   );
 }
